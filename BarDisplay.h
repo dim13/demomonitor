fe0f9b1286778fda511193d69a3a455632e62623
Dimitri Sokolyuk <sokolyuk@gmail.com>
2023-07-26 18:38:49 +0200

    ...

--- BarDisplay.h
+++ BarDisplay.h
@@ -3,15 +3,10 @@
 
 #include "GraphDisplay.h"
 
-#if 0
-#define XtNspace	"space"
-#define XtCSpace	"Space"
-#endif
 #define XtNdefaultGraphWidth	"defaultGraphWidth"
 #define XtCDefaultGraphWidth	"DefaultGraphWidth"
 #define XtNformat	"format"
 #define XtCFormat	"Format"
-
 
 extern WidgetClass barDisplayObjectClass;
 typedef struct _BarDisplayRec *BarDisplayObject;
